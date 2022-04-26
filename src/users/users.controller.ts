46768bbb75db743ad68951f3dabe20d44fbe1561
ayoubmehd <safi-_-gio-raja@live.fr>
2022-04-26 12:31:02 +0000

    refactor: clean-up users controller > use serializer in users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,43 +6,38 @@ import {
   Patch,
   Post,
   Param,
-  UseInterceptors,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
-import MongooseClassSerializerInterceptor from '../interceptors/mongooseClassSerializer.interceptor';
-import { User } from './users.schema';
+import { LocalAuthGuard } from '../auth/local-auth.guard';
 
+@UseGuards(LocalAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseInterceptors(MongooseClassSerializerInterceptor(User))
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @UseInterceptors(MongooseClassSerializerInterceptor(User))
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
-  @UseInterceptors(MongooseClassSerializerInterceptor(User))
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
-  @UseInterceptors(MongooseClassSerializerInterceptor(User))
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
-  @UseInterceptors(MongooseClassSerializerInterceptor(User))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
+import { UsersService } from './users.service';
 import { User, UserSchema } from './users.schema';
 import { UsersController } from './users.controller';
+import MongooseClassSerializerInterceptor from '../interceptors/mongooseClassSerializer.interceptor';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MongooseClassSerializerInterceptor(User),
+    },
+  ],
   exports: [UsersService],
   controllers: [UsersController],
 })
