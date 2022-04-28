d4d34bef08cb4c417e99be9e2c5132fa1172a801
ayoubmehd <safi-_-gio-raja@live.fr>
2022-04-28 09:44:39 +0000

    add joining room

--- src/conference/conference.gateway.ts
+++ src/conference/conference.gateway.ts
@@ -1,9 +1,33 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayConnection,
+  OnGatewayInit,
+  OnGatewayDisconnect,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class ConferenceGateway {
+export class ConferenceGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
   }
+
+  afterInit() {
+    this.server.on('connection', (socket) => {
+      socket.on('new-user', (id: string) => {
+        socket.broadcast.emit('new-peer', id);
+      });
+    });
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {}
+
+  handleDisconnect(client: Socket) {}
 }
