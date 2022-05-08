e417dba12d499611db9245b54c4cf84aa3588138
ayoubmehd <safi-_-gio-raja@live.fr>
2022-05-08 16:03:37 +0100

    use envirement variable for the port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('PORT') || 8000;
+  await app.listen(PORT);
 }
 bootstrap();
